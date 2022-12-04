5115df9d65709f87d120f0593503fe71005d1771
Akindats1 <akindats1@gmail.com>
2022-12-04 09:37:24 +0100

    Third Test

--- StringManipulationApp/Helper.cs
+++ StringManipulationApp/Helper.cs
@@ -41,21 +41,22 @@ namespace StringManipulationApp
                 var result = newA + "" + newB;
                 Console.WriteLine(result);
 
-                
-                
+            }
+
                 int decodeFirstLength = half / 2;
                 int decodeSecondLength = length - decodeFirstLength ;
                 char[] decodedfirstStr = firstArray;
                 char[] decodedSecondStr = secondArray;
                 string[] decodedResult = new string[] {new string(decodedfirstStr), new string(decodedSecondStr)};
 
-                foreach (var item in decodedResult)
+                string outcome = String.Join(" ", decodedResult);
+
+                foreach (var item in outcome)
                 {
-                    Console.WriteLine(item);
+                   
+                   Console.Write(item);
                 }
 
-               
-            }
 
 
 
